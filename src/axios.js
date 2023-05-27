038fe65158814c38dafc6b2e7dbd6fb808a2cef9
Danila Abramov <67269761+danilaabramov@users.noreply.github.com>
2023-05-27 13:56:26 +0400

    Add files via upload

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
     baseURL: process.env.REACT_APP_API_URL ? process.env.REACT_APP_API_URL :
-        'https://vill-mill.ru/api/',
+        'https://api.vill-mill.ru/api/',
         //"https://api.farm-food.site/api/",
         // "https://api-vill-mill.abramov.pw/api/",
         //"http://localhost:5003/api/",
